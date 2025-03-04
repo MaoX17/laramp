acda681d03f59261d1119eaf8fbc6b947f01ed4c
Maurizio Proietti <maurizio.proietti@gmail.com>
2025-03-04 09:48:28 +0100

    add second

--- update.sh
+++ update.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#cd /opt/docker-compose/jellyfin/
 docker compose down
 rm src.backup_pre_build.tar.gz
 tar -czvf src.backup_pre_build.tar.gz src
 sleep 2
-#docker-compose up -d  homeassistant
 docker compose up --build
 sleep 2
 docker compose down
